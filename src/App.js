0cf9e2f96d064e55fca327e599ecaca6bc342ba3
Andrey Arias <devx13@users.noreply.github.com>
2021-07-15 16:27:30 -0700

    added initial routing to app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import './App.css';
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
+
+// TODO: remove after testing
+const TestPage = () => (
+  <div>
+    <h1>HELLO</h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop/hats' component={TestPage} />
+        <Route path='/shop/jackets' component={TestPage} />
+        <Route path='/shop/sneakers' component={TestPage} />
+        <Route path='/shop/womens' component={TestPage} />
+        <Route path='/shop/mens' component={TestPage} />
+
+      </Switch>
     </div>
   );
 }
