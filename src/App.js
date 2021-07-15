6a9d4318c0d6be99ef33381b4c5aa91606c7a2ef
Andrey Arias <devx13@users.noreply.github.com>
2021-07-15 10:26:43 -0700

    refactored components into separate files and updated styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import React from 'react';
 
-import HomePage from './components/homepage/homepage.component';
+import HomePage from './pages/homepage/homepage.component';
 
 function App() {
   return (
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 
 import './menu-item.styles.scss';
 
-const MenuItem = (props) => (
-    <div className='menu-item'>
+const MenuItem = ({ title, imageUrl, size }) => (
+    <div className={`${size} menu-item`}>
+        <div className='background-image' style={{backgroundImage: `url(${imageUrl})`}}></div>
         <div className='content'>
-            <h1 className='title'>{props.title}</h1>
+            <h1 className='title'>{title.toUpperCase()}</h1>
             <span className='subtitle'>SHOP NOW</span>
         </div>
     </div>
