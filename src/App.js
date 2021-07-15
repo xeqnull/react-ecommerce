68224536e802abdcd83b31c20b095b82e0e6ad80
Andrey Arias <devx13@users.noreply.github.com>
2021-07-14 19:22:49 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import React from 'react';
+
+import HomePage from './components/homepage/homepage.component';
+
+function App() {
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
