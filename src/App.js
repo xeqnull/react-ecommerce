ac8f60f1ac218cf331875a258fd5e71f05dc000f
Andrey Arias <devx13@users.noreply.github.com>
2021-07-17 16:41:18 -0700

    added header component and logo to app

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Switch, Route } from 'react-router-dom';
 
 import HomePage from './pages/homepage/home.component';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
 // TODO: remove after testing
 const TestPage = () => (
@@ -15,6 +16,7 @@ const TestPage = () => (
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route exact path='/shop' component={ShopPage} />
